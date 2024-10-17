974185650521a32d0edb71f7aa6f1796767975a2
LukaszOstr <147150234+LukaszOstr@users.noreply.github.com>
2024-10-17 14:58:17 +0200

    Add files via upload

--- zestaw3.java
+++ zestaw3.java
@@ -1,6 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
 public class zestaw3 {
+
+
+
 
     static void trojkiPitagorejskie(int n){
         for(int i=1; i<=n; i++){
@@ -101,13 +106,259 @@ public class zestaw3 {
 
     }
 
+    static int silnia(int n){
+        int silnia=1;
+        for(int i=1;i<=n; i++){
+            silnia *= i;
+        }
+        return silnia;
+    }
+
+    static void silniaPodwojna(int n){
+        int silnia=1;
+        if(n%2== 1){
+            for(int i=1;i<=n; i=i+2){
+                silnia *= i;
+            }
+            System.out.println(silnia);
+        }
+        else{
+            for(int i=2;i<=n; i=i+2){
+                silnia *= i;
+            }
+            System.out.println(silnia);
+        }
+    }
+
+    static void silnia(int m, int n){
+        int silnia=1;
+        for(int i=n;i>=1; i=i-m){
+            silnia *= i;
+        }
+        System.out.println(silnia);
+    }
+
+    static void dwumianNewtona(int n, int k){
+        System.out.println(silnia(n) / (silnia(k) * silnia(n - k)));
+    }
+
+    static void ciagFibonacciego(int n){
+        int fib1=1;
+        int fib2=1;
+        int fib3=1;
+
+        if(n == 1){
+            System.out.print("1");
+        }
+        else {
+            System.out.print("1 1 ");
+
+            for (int i = 3; i <= n; i++) {
+                fib3 = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = fib3;
+                System.out.print(fib3 + " ");
+            }
+        }
+    }
+
+    static int sumaNaturalnych(int n){
+
+        Scanner scan = new Scanner(System.in);
+        int suma = 0;
+        for(int i=1; i<=n; i++){
+            int temp = scan.nextInt();
+            if(temp<0){
+                i--;
+            }
+            else{
+                suma += temp;
+            }
+
+        }
+        return suma;
+    }
+
+    static int sumaParzystych(int n){
+        Scanner scan = new Scanner(System.in);
+        int suma = 0;
+        for(int i=1; i<=n; i++){
+            int temp = scan.nextInt();
+            if(temp%2==0){
+                suma += temp;
+            }
+            else{
+                i--;
+            }
+
+        }
+        return suma;
+    }
+
+    static int sumaNieparzystych(int n){
+        Scanner scan = new Scanner(System.in);
+        int suma = 0;
+        for(int i=1; i<=n; i++){
+            int temp = scan.nextInt();
+            if(temp%2==1){
+                suma += temp;
+            }
+            else{
+                i--;
+            }
+
+        }
+        return suma;
+    }
+
+    static int sumaKwaNaturalnych(int n){
+
+        Scanner scan = new Scanner(System.in);
+        int suma = 0;
+        for(int i=1; i<=n; i++){
+            int temp = scan.nextInt();
+            if(temp<0){
+                i--;
+            }
+            else{
+                suma += temp*temp;
+            }
+
+        }
+        return suma;
+    }
+
+    static int sumaSzeNaturalnych(int n){
+
+        Scanner scan = new Scanner(System.in);
+        int suma = 0;
+        for(int i=1; i<=n; i++){
+            int temp = scan.nextInt();
+            if(temp<0){
+                i--;
+            }
+            else{
+                suma += temp*temp*temp;
+            }
+
+        }
+        return suma;
+    }
+
+    static double sumaOdwNaturalnych(int n){
+
+        Scanner scan = new Scanner(System.in);
+        double suma = 0;
+        for(int i=1; i<=n; i++){
+            int temp = scan.nextInt();
+            if(temp<0){
+                i--;
+            }
+            else{
+                suma += (double) 1 / temp;
+            }
+
+        }
+        return suma;
+    }
+
+    static boolean czyPalindrom(String wyraz){
+        for(int i=0; i<=(wyraz.length())/2; i++){
+            if(wyraz.charAt(i) != wyraz.charAt(wyraz.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean czyDoskonala(int n){
+        int suma=0;
+
+        for(int i=1;i<n;i++){
+            if(n%i == 0){
+                suma += i;
+            }
+        }
+        if(suma == n){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    static boolean czyPierwsza(int n){
+
+        for(int i=2;i<n;i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static int NWD(int a, int b){
+        if(a > b){
+            for(int i=a/2; i>0; i--){
+                if(a%i == 0 && b%i == 0){
+                    return i;
+                }
+            }
+        }
+        else if (b > a) {
+            for(int i=b/2; i>0; i--){
+                if(a%i == 0 && b%i == 0){
+                    return i;
+                }
+            }
+        }
+        else {
+            return a;
+        }
+        return 1;
+    }
+
+    static int[] wczytajTablice(){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int[] tab = new int[n];
+
+        for(int i=0; i<n; i++){
+            int temp = scan.nextInt();
+            tab[i] = temp;
+        }
+         return tab;
+
+    }
+
+    static void podzbiory(int[] tab){
+        int n = tab.length;
+        //System.out.print("pusty");
+        for(int i=0; i < (1<<n); i++){
+            System.out.print("{ ");
+            for(int j=0; j < n; j++){
+                if((i & (1 << j)) > 0){
+                    System.out.print(tab[j] + " ");
+                }
+
+            }
+            System.out.println("}");
+        }
+
+
+    }
 
 
 
 
 
     public static void main(String[] args) {
-        piramida(3,2);
+        //wczytajTablice();
+        int[] w = {1, 2, 3};
+        //System.out.print();
+        podzbiory(w);
 
 
     }
